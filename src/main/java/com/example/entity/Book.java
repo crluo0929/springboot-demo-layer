a27f2be01e9fe39f50279ff431b64a8c1287b181
smartluo0929 <smartluo0929@outlook.com>
2021-01-12 13:40:10 +0800

    demo for multi layer

---
+++ src/main/java/com/example/entity/Book.java
@@ -0,0 +1,58 @@
+package com.example.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
+	private int id ;
+	private String title ;
+	private String isbn ;
+	private String author ;
+	private String lang ;
+	private Date publish ;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getLang() {
+		return lang;
+	}
+	public void setLang(String lang) {
+		this.lang = lang;
+	}
+	public Date getPublish() {
+		return publish;
+	}
+	public void setPublish(Date publish) {
+		this.publish = publish;
+	}
+	
+}
